9af590309ba178f1d9aa5faf204555b3eb72c93b
Douglas Albuquerque <douglasalbuquerque95@gmail.com>
2016-04-09 00:03:02 -0300

    Inserir novos dados no dataset e implementacao dos testes de aluno e professor

--- src/test/java/com/softwarecorporativo/monitoriaifpe/aluno/TesteAluno.java
+++ src/test/java/com/softwarecorporativo/monitoriaifpe/aluno/TesteAluno.java
@@ -11,10 +11,9 @@ import com.softwarecorporativo.monitoriaifpe.instituto.curso.Curso;
 import com.softwarecorporativo.monitoriaifpe.instituto.disciplina.Disciplina;
 import com.softwarecorporativo.monitoriaifpe.util.Util;
 import com.softwarecorporativo.monitoriaifpe.util.constantes.Constantes;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
@@ -43,14 +42,51 @@ public class TesteAluno extends MonitoriaTestCase {
 
     }
 
+    @Test
+    public void testeUpdateAluno() {
+        LOGGER.log(Level.INFO, "Teste de Atualização Email de um Aluno - {1}", name.getMethodName());
+        try {
+            //super.entityTransaction.begin();
+            Aluno alunoBuscado = super.entityManager.find(Aluno.class, 1L);
+            String emailAntigo = alunoBuscado.getEmail();
+            alunoBuscado.setEmail("edmilson@hotmail.com");
+            super.entityTransaction.commit();
+            assertEquals(true, true);
+        } catch (Exception e) {
+            if (entityTransaction != null && entityTransaction.isActive()) {
+                LOGGER.log(Level.SEVERE, e.getMessage(), e);
+                entityTransaction.rollback();
+            }
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testeDeleteAluno() {
+        LOGGER.log(Level.INFO, "Teste de Remoção de um Aluno - {2}", name.getMethodName());
+        try {
+            Aluno alunoBuscado = super.entityManager.find(Aluno.class, 3L);
+            alunoBuscado = super.entityManager.merge(alunoBuscado);
+            super.entityManager.remove(alunoBuscado);
+            super.entityTransaction.commit();
+            assertEquals(true, true);
+        } catch (Exception e) {
+            if (entityTransaction != null && entityTransaction.isActive()) {
+                LOGGER.log(Level.SEVERE, e.getMessage(), e);
+                entityTransaction.rollback();
+            }
+            Assert.fail(e.getMessage());
+        }
+    }
+
     private Aluno montarObjetoAluno() {
         Aluno aluno_criado = new Aluno();
         aluno_criado.setNome("Fulano");
-        aluno_criado.setSobrenome("Santana");
-        aluno_criado.setEmail("douglasalbuquerque@gmail.com");
-        aluno_criado.setLogin("doug21");
+        aluno_criado.setSobrenome("Silva");
+        aluno_criado.setEmail("fulano@gmail.com");
+        aluno_criado.setLogin("fulano21");
         aluno_criado.setMatricula("20142Y6-RC2222");
-        String password = Util.criptografarSenha("sport", "sport", Constantes.CONSTANTE_CRIPTOGRAFIA);
+        String password = Util.criptografarSenha("123", "123", Constantes.CONSTANTE_CRIPTOGRAFIA);
         aluno_criado.setSenha(password);
         Curso curso = super.entityManager.find(Curso.class, 1L);
         aluno_criado.setCurso(curso);
