52bcff3f6bfc57f843a37802a86917026f27b21d
Douglas Albuquerque <douglasalbuquerque95@gmail.com>
2016-04-21 21:46:38 -0300

    Inserindo algumas annotatios Bean na classe usuario

--- src/main/java/com/softwarecorporativo/monitoriaifpe/usuario/Usuario.java
+++ src/main/java/com/softwarecorporativo/monitoriaifpe/usuario/Usuario.java
@@ -12,6 +12,11 @@ import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 @Table(name = "TB_USUARIO")
@@ -22,19 +27,28 @@ import javax.persistence.Table;
 @Access(AccessType.FIELD)
 public abstract class Usuario extends EntidadeNegocio  {
 
+    @NotNull
     @Column(name = "USUARIO_NOME", nullable = false)
     private String nome;
-
+    
+    @NotNull
     @Column(name = "USUARIO_SOBRENOME", nullable = false)
     private String sobrenome;
-
+    
+    @NotBlank
     @Column(name = "USUARIO_LOGIN", nullable = false)
     private String login;
-
+    
+    @NotBlank
+//    @Size(min = 4, max = 10)
+//    @Pattern(regexp = "((?=.*\\p{Digit})(?=.*\\p{Lower}) (?=.*{Upper}) . {4,10})", 
+//            message = "Senha não se enquadra no padrão estabelecido!")
     @Column(name = "USUARIO_SENHA", nullable = false)
     private String senha;
-
-    @Column(name = "USUARIO_EMAIL", nullable = false)
+    
+    @NotBlank
+    @Email
+    @Column(name = "USUARIO_EMAIL", length = 30 ,nullable = false)
     private String email;
 
    
