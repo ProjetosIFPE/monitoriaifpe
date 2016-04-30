74bc4006c07a39e7a9f9a8b12a1b4899358a4780
Edmilson <edmilsonsantana2@hotmail.com>
2016-04-30 07:34:56 -0300

    Corrigindo Problema na validação da atividade

--- src/main/java/com/softwarecorporativo/monitoriaifpe/atividade/validation/ValidadorAtividade.java
+++ src/main/java/com/softwarecorporativo/monitoriaifpe/atividade/validation/ValidadorAtividade.java
@@ -6,6 +6,9 @@
 package com.softwarecorporativo.monitoriaifpe.atividade.validation;
 
 import com.softwarecorporativo.monitoriaifpe.atividade.Atividade;
+import com.softwarecorporativo.monitoriaifpe.util.Util;
+import java.util.Calendar;
+import java.util.Date;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
@@ -17,12 +20,29 @@ public class ValidadorAtividade implements ConstraintValidator<ValidaAtividade, 
 
     @Override
     public void initialize(ValidaAtividade validaAtividade) {
-        
+
     }
 
     @Override
     public boolean isValid(Atividade atividade, ConstraintValidatorContext cvc) {
         if (atividade != null && atividade.getHorarioEntrada() != null && atividade.getHorarioSaida() != null) {
+            
+            Calendar dataHorarioEntrada = Calendar.getInstance();
+            Calendar dataHorarioSaida = Calendar.getInstance();
+            dataHorarioSaida.setTime(atividade.getHorarioSaida());
+            dataHorarioEntrada.setTime(atividade.getHorarioEntrada());
+
+            Calendar dataAtual = Calendar.getInstance();
+            dataAtual.set(Calendar.HOUR_OF_DAY, dataHorarioEntrada.get(Calendar.HOUR_OF_DAY));
+            dataAtual.set(Calendar.MINUTE, dataHorarioEntrada.get(Calendar.MINUTE));
+            dataAtual.set(Calendar.SECOND, dataHorarioEntrada.get(Calendar.SECOND));
+            atividade.setHorarioEntrada(dataAtual.getTime());
+            
+            dataAtual.set(Calendar.HOUR_OF_DAY, dataHorarioSaida.get(Calendar.HOUR_OF_DAY));
+            dataAtual.set(Calendar.MINUTE, dataHorarioSaida.get(Calendar.MINUTE));
+            dataAtual.set(Calendar.SECOND, dataHorarioSaida.get(Calendar.SECOND));
+            atividade.setHorarioSaida(dataAtual.getTime());
+            
             return atividade.getHorarioEntrada().before(atividade.getHorarioSaida());
         }
         return Boolean.FALSE;
