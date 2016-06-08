3273b4d08d5a2c34531219d08bd3802e2cc37dea
Douglas Albuquerque <douglasalbuquerque95@gmail.com>
2016-06-08 08:14:21 -0300

    Implementando tela de cadastro de disciplina*

--- src/main/java/com/softwarecorporativo/monitoriaifpe/bean/DisciplinaBean.java
+++ src/main/java/com/softwarecorporativo/monitoriaifpe/bean/DisciplinaBean.java
@@ -5,19 +5,38 @@
  */
 package com.softwarecorporativo.monitoriaifpe.bean;
 
+import com.softwarecorporativo.monitoriaifpe.modelo.disciplina.ComponenteCurricular;
 import com.softwarecorporativo.monitoriaifpe.modelo.disciplina.Disciplina;
+import com.softwarecorporativo.monitoriaifpe.modelo.professor.Professor;
+import com.softwarecorporativo.monitoriaifpe.servico.ComponenteCurricularService;
 import com.softwarecorporativo.monitoriaifpe.servico.DisciplinaService;
+import com.softwarecorporativo.monitoriaifpe.servico.ProfessorService;
+import java.util.List;
 import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 /**
  *
  * @author Edmilson Santana
  */
+@ManagedBean
+@ViewScoped
 public class DisciplinaBean extends GenericBean<Disciplina> {
     
     @EJB
     private DisciplinaService disciplinaService;
+    
+    @EJB
+    private ComponenteCurricularService componenteCurrService;
 
+    @EJB
+    private ProfessorService professorService;
+    
+    private Long professorId;
+    
+    private Long componenteCurricularID;
+    
     @Override
     void inicializarEntidadeNegocio() {
         super.setEntidadeNegocio(disciplinaService.getEntidadeNegocio());
@@ -28,5 +47,40 @@ public class DisciplinaBean extends GenericBean<Disciplina> {
         setService(disciplinaService);
     }
     
+    public List<ComponenteCurricular> listarComponentesCurriculares(){
+        return this.componenteCurrService.listarTodos();
+    }
+    
+    public List<Professor> listarProfessores(){
+        return this.professorService.listarTodos();
+    }
+
+    public Long getProfessorId() {
+        return professorId;
+    }
+
+    public void setProfessorId(Long professorId) {
+        this.professorId = professorId;
+    }
+
+    public Long getComponenteCurricularID() {
+        return componenteCurricularID;
+    }
+
+
+    public void setComponenteCurricularID(Long componenteCurricularID) {
+        this.componenteCurricularID = componenteCurricularID;
+    }
+
+    
+    
+    @Override
+    public void gravar(){
+        Professor professor = professorService.buscarEntidade(professorId);
+        ComponenteCurricular componenteCurricular = componenteCurrService.buscarEntidade(componenteCurricularID);
+        entidadeNegocio.setProfessor(professor);
+        entidadeNegocio.setComponenteCurricular(componenteCurricular);
+        super.gravar();
+    }
     
 }
--- src/main/java/com/softwarecorporativo/monitoriaifpe/modelo/util/Util.java
+++ src/main/java/com/softwarecorporativo/monitoriaifpe/modelo/util/Util.java
@@ -1,12 +1,15 @@
 package com.softwarecorporativo.monitoriaifpe.modelo.util;
 
+import com.softwarecorporativo.monitoriaifpe.modelo.negocio.EntidadeNegocio;
+import com.softwarecorporativo.monitoriaifpe.modelo.periodo.Periodo;
+import com.softwarecorporativo.monitoriaifpe.modelo.util.constantes.Semestre;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Locale;
 
 public class Util {
-
 
     public static String formatarData(Date data) {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy", new Locale("pt", "br"));
@@ -20,13 +23,30 @@ public class Util {
         c.set(Calendar.DAY_OF_MONTH, dia);
         return c.getTime();
     }
-    
+
     public static Date getTime(Integer horas, Integer minutos, Integer segundos) {
         Calendar c = Calendar.getInstance();
         c.set(Calendar.HOUR_OF_DAY, horas);
         c.set(Calendar.MINUTE, minutos);
         c.set(Calendar.SECOND, segundos);
         return c.getTime();
+    }
+
+    public static Periodo getSemestreAtual() {
+        Periodo periodo = new Periodo();
+        Calendar dataAtual = Calendar.getInstance();
+        int anoAtual = dataAtual.get(Calendar.YEAR);
+        Calendar inicioSegundoSemestre = Calendar.getInstance();
+        inicioSegundoSemestre.set(anoAtual, 7, 1);
+        if (dataAtual.after(inicioSegundoSemestre)) {
+            periodo.setSemestre(Semestre.SEGUNDO);
+        } else {
+            periodo.setSemestre(Semestre.PRIMEIRO);
+        }
+        periodo.setAno(anoAtual);
+
+        return periodo;
+
     }
 
 }
